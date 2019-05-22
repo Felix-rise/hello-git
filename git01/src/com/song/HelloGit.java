db8a949eed02ad024a98137a70d064468b894a23
Felix <1156353764@qq.com>
2019-05-22 10:22:54 +0800

    第一次提交源代码

--- git01/src/com/song/HelloGit.java
+++ git01/src/com/song/HelloGit.java
@@ -7,5 +7,9 @@ public class HelloGit {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git!");
+
+
+        System.out.println("嘿嘿嘿嘿嘿嘿嘿");
+        System.out.println("哈哈哈哈哈哈哈");
     }
 }
