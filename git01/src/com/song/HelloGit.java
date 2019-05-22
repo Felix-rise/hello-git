20d8c6f034a787a28dd2ae9442dec5e7794e1de8
Felix <1156353764@qq.com>
2019-05-22 10:47:23 +0800

    克隆下来的第一次修改

--- git01/src/com/song/HelloGit.java
+++ git01/src/com/song/HelloGit.java
@@ -8,8 +8,15 @@ public class HelloGit {
         System.out.println("hello git!");
         System.out.println("hello git!");
 
+        //Hello
 
         System.out.println("嘿嘿嘿嘿嘿嘿嘿");
         System.out.println("哈哈哈哈哈哈哈");
+
+        //git
+    }
+
+    public void sayHello() {
+        System.out.println("hello");
     }
 }
